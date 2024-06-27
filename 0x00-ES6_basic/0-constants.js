008a9644643ddba7c4b5f3b1c58e6c971d95bdcc
NdigirigiGichuhi <ndigirigigichuhi@gmail.com>
2024-06-27 12:12:13 +0300

    zero

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,9 +1,9 @@
-taskFirst() {
+export function taskFirst() {
     const task = 'I prefer const when I can.';
     return task;
 }
 
-function taskNext() {
+export function taskNext() {
     let combination = 'But sometimes lry';
     combination += getLast();
 
