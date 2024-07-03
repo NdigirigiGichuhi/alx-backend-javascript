3b9afd8281ec8580a0f90cdfc45b12b3e60c0ad2
NdigirigiGichuhi <ndigirigigichuhi@gmail.com>
2024-07-04 01:21:32 +0300

    3

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,13 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  createUser()
+    .then((user) => {
+      uploadPhoto()
+        .then((photo) => {
+          console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+        });
+    }).catch(() => {
+      console.error(Error('Signup system offline'));
+    });
+}
