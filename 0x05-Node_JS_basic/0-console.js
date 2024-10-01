c3d25cbc83b04bb2c47981da80375f71c4900019
NdigirigiGichuhi <ndigirigigichuhi@gmail.com>
2024-10-01 20:13:56 +0300

    fix question 0

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,10 +1,9 @@
 /**
  * Prints the given message to STDOUT.
  *
- * @param {msg} str - The message to be printed.
+ * @param {string} str - The message to be printed.
  */
-
-function displayMessage (string) {
+function displayMessage(string) {
     console.log(string);
 }
 
